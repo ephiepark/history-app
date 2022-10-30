ce3f35b31f28ff3b1ff21532066cb003666fb0c6
Ephraim Park <ephiepark@gmail.com>
2022-10-30 11:51:26 -0700

    move add timeline button to the top

--- src/components/Timeline/Home.tsx
+++ src/components/Timeline/Home.tsx
@@ -7,19 +7,19 @@ export default () => {
   const addTimelineButton = timelineKeys.length >= 3 ? null : <Button onClick={() => {
     setTimelineKeys([...timelineKeys, `${Math.floor(Date.now() / 1000)}`]);
   }}>Add Timeline</Button>;
-  return <Grid container spacing={2}>
-    {timelineKeys.map((timelineKey) => {
-      return <Grid key={timelineKey} item xs={11 / timelineKeys.length}>
-        <Stack>
-          <Button onClick={() => {
-            setTimelineKeys(timelineKeys.filter((timelineKeyIt) => timelineKeyIt !== timelineKey))
-          }}>Remove</Button>
-          <Timeline />
-        </Stack>
-      </Grid>;
-    })}
-    <Grid item xs={1}>
-      {addTimelineButton}
+  return <Stack>
+    {addTimelineButton}
+    <Grid container spacing={2}>
+      {timelineKeys.map((timelineKey) => {
+        return <Grid key={timelineKey} item xs={Math.floor(12 / timelineKeys.length)}>
+          <Stack>
+            <Button onClick={() => {
+              setTimelineKeys(timelineKeys.filter((timelineKeyIt) => timelineKeyIt !== timelineKey))
+            }}>Remove</Button>
+            <Timeline />
+          </Stack>
+        </Grid>;
+      })}
     </Grid>
-  </Grid>;
+  </Stack>;
 };
